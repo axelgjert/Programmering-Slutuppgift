4045dfb1b2e377a33306697f602fe80f0ef3f507
axelgjert <axel.gjertsson@elev.ga.ntig.se>
2021-05-19 13:18:42 +0200

    ds

--- CoronPROG/VaccinationByLand.py
+++ CoronPROG/VaccinationByLand.py
@@ -1,14 +1,16 @@
+#Moduler som krävs för programmet
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from matplotlib.dates import DateFormatter, WeekdayLocator 
 from datetime import timedelta
 
+#Läser in en csv fil med vaccinationsstatus från nätet
 df = pd.read_csv(
     'https://covid.ourworldindata.org/data/owid-covid-data.csv', 
-    usecols=['date', 'location', 'total_vaccinations_per_hundred'], 
+    usecols=['date', 'location', 'total_vaccinations_per_hundred'], #"Titlar" inuti csv filen
     parse_dates=['date'])
 
-countries = ['United States', 'China', 'United Kingdom', 'Sweden']
+countries = ['Sweden'] #Här väljer vi vilket land vi vill undersöka från titeln 'location'
 df = df[df['location'].isin(countries)]
 
 pivot = pd.pivot_table(
@@ -20,10 +22,6 @@ pivot = pd.pivot_table(
     )
 
 pivot = pivot.fillna(method='ffill')
-
-main_country = 'United States'
-colors = {country:('grey' if country!= main_country else '#129583') for country in countries}
-alphas = {country:(0.75 if country!= main_country else 1.0) for country in countries}
 
 # Step 4: Plot all countries
 fig, ax = plt.subplots(figsize=(12,8))
@@ -48,7 +46,7 @@ for country in countries:
 
 #Formaterar vad som ska visas
 date_form = DateFormatter("%Y-%m-%d")
-ax.xaxis.set_major_locator(WeekdayLocator(byweekday=(0), interval=1))
+ax.xaxis.set_major_locator(WeekdayLocator(byweekday=(0), interval=1)) #Vilket intervall som diagramet läser in. I vårat vall varje dag.
 ax.xaxis.set_major_formatter(date_form)
 plt.xticks(rotation=45)
 plt.ylim(0,100)
@@ -64,7 +62,7 @@ ax.grid(alpha=0.1)
 #Namnger axlarna och ger storlek på texten
 plt.ylabel('Vaccination per 100 personer', fontsize=12, alpha=0.9)
 plt.xlabel('Datum', fontsize=12, alpha=0.9)
-plt.title('COVID-19 Vaccinationer över Tid', fontsize=18, weight='bold', alpha=0.9)
+plt.title('COVID-19 Vaccinationer över Tid', fontsize=20, weight='bold', alpha=0.9)
 
-# Visar våran fina plot
+# Visar våran graf
 plt.show()
