1f9cb61bcaed710a23d56947082d8202b6c47b48
axelgjert <axel.gjertsson@elev.ga.ntig.se>
2021-05-20 08:33:21 +0200

    jijti

---
+++ CoronaDeaths.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import plotly.express as px
+
+
+df = pd.read_csv("deaths.csv")
+
+datum = df['Date']
+antal_döda = df['National_Daily_Deaths']
+
+fig = px.line(x = datum, y = antal_döda, title = 'Antal döda under en tidsperiod', labels = {'x':'Månad', 'y':'Död'})
+pig = px.scatter(x = datum, y = antal_döda, title = 'Antal döda under en tidsperiod', labels = {'x':'Månad', 'y':'Död'})
+
+fig.show()
+
+
+
+
+
